ff9f7ea9414304eeb59ba6db68af0cd6774ec485
rniradiel <rniradiel@gmail.com>
2024-12-11 12:12:13 +0800

    make the year dynamic

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import styles from './FooterStyles.module.css';
 
 function Footer() {
+  const year = new Date().getFullYear();
+
   return (
     <section id="footer" className={styles.container}>
       <p>
-        &copy; 2024 Reyschelle Iradiel. <br />
-        All rights reserved.
+        All rights reserved &copy; {year}
       </p>
     </section>
   );
